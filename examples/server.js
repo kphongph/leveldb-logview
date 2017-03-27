b6e8ee0ace6d5c8e032869408c22e38fdb36e95a
Theerawut <Theerawutt53@gmail.com>
2017-03-27 21:48:15 +0700

    add Promise

--- examples/server.js
+++ examples/server.js
@@ -3,16 +3,22 @@ var JSONStream = require('JSONStream');
 var logview = require('..');
 var config = require('./config');
 
-var stream = logview('./test');
+var stream = logview({
+  db:{path:'./test'}
+});
 
 stream.on('insert',function(d) {
+ console.log(d.value.CODE);
+});
+
+stream.on('update',function(d) {
  console.log(d.value.CODE);
 });
 
 
 request.get({
   url:'http://maas.nuqlis.com:44300/log/dmc59_1',
-  qs:{'start':'1490593000000','limit':10},
+  qs:{'start':'1490593619426','limit':'1'},
   headers: {
     'authorization':'JWT '+config.token
   }})
