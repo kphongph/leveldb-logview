fed14e628483afdf79b9e2ec60842beab80d6623
kphongph <kphongph@gmail.com>
2017-06-09 23:00:09 +0700

    add background

--- index.js
+++ index.js
@@ -10,12 +10,11 @@ var locks = require('locks');
 
 var logview = exports;
 
-var _revSize = 100;
-
 var mutex = locks.createMutex();
 
 logview.config = function(config) {
   logview.config = config;
+  logview.config.batchSize = config.batchSize?config.batchSize:10;
   var dbPath = config.dbPath?config.dbPath:'_view'; 
   logview.config.db = sublevel(levelup(dbPath,{'valueEncoding':'json'}));
   logview.config.configDb = logview.config.db.sublevel('_config'); 
@@ -45,7 +44,7 @@ var _updateMirror = function(self,db,chunk,cb) {
 var _request = function(config,_rev,callback) {
   var stream = request.get({
     url:config.url,
-    qs:{'gt':_rev,'limit':_revSize},
+    qs:{'gt':_rev,'limit':config.batchSize},
     headers: {
      'authorization':'JWT '+config.jwtToken
     }
@@ -61,12 +60,22 @@ var _request = function(config,_rev,callback) {
   }
 
   stream.pipe(through2.obj(function(chunk,enc,cb) {
-   // console.log('put',chunk._rev);
     config.configDb.put('_rev',{'ts':chunk._rev});
     cb();
   })).on('finish',function() {
-   // console.log('finish');
     callback();
+  });
+};
+
+logview.sync = function(cb) {
+  var config = logview.config;
+  var configDb = config.configDb;
+  configDb.get('_rev',function(err,value) {
+    if(err) {
+      _request(config,'0',cb);
+    } else {
+      _request(config,value.ts,cb);
+    }
   });
 };
 
